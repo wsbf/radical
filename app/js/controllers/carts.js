05d16695e34b7645be0740c8b7b866abc1420b48
Ben Shealy <bentshermann@gmail.com>
2017-04-09 12:19:11 -0400

    Added enqueue button to carts view

--- app/js/controllers/carts.js
+++ app/js/controllers/carts.js
@@ -4,10 +4,11 @@ var cartModule = angular.module("wizbif.carts", [
 	"ui.router",
 	"ui.bootstrap",
 	"wizbif.alert",
-	"wizbif.database"
+	"wizbif.database",
+	"wizbif.queue"
 ]);
 
-cartModule.controller("CartsCtrl", ["$scope", "$stateParams", "db", function($scope, $stateParams, db) {
+cartModule.controller("CartsCtrl", ["$scope", "$stateParams", "alert", "db", "queue", function($scope, $stateParams, alert, db, queue) {
 	$scope.cart_types = db.getDefs("cart_type");
 	$scope.cart_typeID = $stateParams.cart_typeID;
 	$scope.carts = [];
@@ -22,6 +23,18 @@ cartModule.controller("CartsCtrl", ["$scope", "$stateParams", "db", function($sc
 						return (cart.end_date == null || now < Date.parse(cart.end_date));
 					});
 			});
+	};
+
+	$scope.enqueueCart = function(cart) {
+		var item = {
+			album_code: cart.cartID,
+			rotation: $scope.cart_types[cart.cart_typeID].type,
+			track_name: cart.title,
+			artist_name: cart.issuer
+		};
+
+		queue.insert(item);
+		alert.success("Added cart to play queue.");
 	};
 
 	// initialize
